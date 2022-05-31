7ec2664e19187fd7a33c5e20e067b91566263d71
TamaraZolnajic <zolnajic.tamara@gmail.com>
2022-05-31 16:10:02 +0200

    challenge finished

--- tamara/challenge/script.js
+++ tamara/challenge/script.js
@@ -1,8 +1,22 @@
+let title= document.getElementById("title")
 var students = ["John", "Jane", "Mary", "Lucas", "Jack"];
-var mathGrades = [70, 85, 65, 30, 100];
-var result = prompt("type your name");
-if (students.includes(result)) {
-    document.write(students[0] + " has reached " + mathGrades[0] + " points in Math this season.")
-} else {
-    document.write("You are not on the list.")
-}
+        var mathGrades = [70, 85, 65, 30, 100];
+        var result = prompt("type your name");
+        if (students.includes(result)) {
+            title.innerText = students[students.indexOf(result)] + " has reached " + mathGrades[students.indexOf(result)] + " points in Math this season."
+        } else {
+            title.innerText = "You are not on the list."
+        }
+        if (mathGrades[students.indexOf(result)] < 60) {
+            title = document.getElementById("title").style.color = "red";
+        }else if (mathGrades[students.indexOf(result)] <= 69) {
+            title = document.getElementById("title").style.color = "yellow";
+        }
+        else if (mathGrades[students.indexOf(result)] <100) {
+            title = document.getElementById("title").style.color = "green";
+        }
+        else if (mathGrades[students.indexOf(result)] == 100) {
+            title = document.getElementById("title").style.color = "blue";
+        }
+        
+        
